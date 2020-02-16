073baa6d41afc82f6eab604c8cbaeee99228d9a5
Vadim <vgadaborshev@gmail.com>
2020-02-16 13:03:58 +0300

    Add - some photos on nav1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { NgModule }                from '@angular/core';
 import { NzButtonModule }          from 'ng-zorro-antd/button';
 import { NzMenuModule }            from 'ng-zorro-antd/menu';
 import { NzLayoutModule }          from 'ng-zorro-antd/layout';
+import { NzCardModule }            from 'ng-zorro-antd/card';
 import { AppComponent }            from './app.component';
 import { AppRoutingModule }        from './app-routing.module';
 import { NavOneComponent }         from './navs/nav-one/nav-one.component';
 import { NavTwoComponent }         from './navs/nav-two/nav-two.component';
 import { NavThreeComponent }       from './navs/nav-three/nav-three.component';
 
-const ANT_DESIGN_MODULES = [NzButtonModule, NzLayoutModule, NzMenuModule];
+const ANT_DESIGN_MODULES = [NzButtonModule, NzLayoutModule, NzMenuModule, NzCardModule];
 const BROWSER_MODULES = [BrowserModule, BrowserAnimationsModule];
 
 @NgModule({
--- src/app/navs/nav-one/nav-one.component.ts
+++ src/app/navs/nav-one/nav-one.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-one',
   templateUrl: './nav-one.component.html',
   styleUrls: ['./nav-one.component.css']
 })
-export class NavOneComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class NavOneComponent {
+  photos = [
+    {title: 'ПС 500 кВ Щёлоков', description: '2014 год', path: '1.jpg'},
+    {title: 'asd', description: 'qwe', path: '2.jpg'},
+    {title: 'asd', description: 'qwe', path: '3.jpg'},
+    {title: 'asd', description: 'qwe', path: '4.jpg'},
+    {title: 'asd', description: 'qwe', path: '5.jpg'},
+  ];
 }
