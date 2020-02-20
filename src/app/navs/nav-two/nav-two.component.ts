bda5163358fea32bc8d064f29c7047071c389d1d
Vadim <vgadaborshev@gmail.com>
2020-02-20 22:20:38 +0300

    Add some foto nav2

--- src/app/navs/nav-two/nav-two.component.ts
+++ src/app/navs/nav-two/nav-two.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-nav-two',
   templateUrl: './nav-two.component.html',
   styleUrls: ['./nav-two.component.css']
 })
-export class NavTwoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class NavTwoComponent  {
+  photos = [
+    {title: 'Совсем маленькие', description: '2016 год', path: '1.jpg'},
+    {title: 'Совсем маленькие', description: '2016 год', path: '2.jpg'},
+    {title: 'На прогулке', description: '2017 год', path: '3.jpg'},
+  ];
 }
