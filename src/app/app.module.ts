540c2918d3e4e025471a35dcb973d929a1e8339f
Vadim <vgadaborshev@gmail.com>
2020-02-22 21:33:07 +0300

    Add avatar button '+','-" icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule }           from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule }                from '@angular/core';
+import { HttpClientModule }        from '@angular/common/http';
 import { NzButtonModule }          from 'ng-zorro-antd/button';
 import { NzMenuModule }            from 'ng-zorro-antd/menu';
 import { NzLayoutModule }          from 'ng-zorro-antd/layout';
 import { NzCardModule }            from 'ng-zorro-antd/card';
+import { NzAvatarModule }          from 'ng-zorro-antd/avatar';
+import { NzIconModule }            from 'ng-zorro-antd/icon';
 import { AppComponent }            from './app.component';
 import { AppRoutingModule }        from './app-routing.module';
 import { NavOneComponent }         from './navs/nav-one/nav-one.component';
 import { NavTwoComponent }         from './navs/nav-two/nav-two.component';
 import { NavThreeComponent }       from './navs/nav-three/nav-three.component';
 
-const ANT_DESIGN_MODULES = [NzButtonModule, NzLayoutModule, NzMenuModule, NzCardModule];
+const ANT_DESIGN_MODULES = [
+  NzButtonModule,
+  NzLayoutModule,
+  NzMenuModule,
+  NzCardModule,
+  NzAvatarModule,
+  NzIconModule
+];
 const BROWSER_MODULES = [BrowserModule, BrowserAnimationsModule];
 
 @NgModule({
   declarations: [AppComponent, NavOneComponent, NavTwoComponent, NavThreeComponent],
-  imports: [AppRoutingModule, BROWSER_MODULES, ANT_DESIGN_MODULES],
+  imports: [AppRoutingModule, HttpClientModule, BROWSER_MODULES, ANT_DESIGN_MODULES],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/navs/nav-two/nav-two.component.ts
+++ src/app/navs/nav-two/nav-two.component.ts
@@ -1,14 +1,21 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-two',
   templateUrl: './nav-two.component.html',
   styleUrls: ['./nav-two.component.css']
 })
-export class NavTwoComponent  {
+export class NavTwoComponent {
   photos = [
-    {title: 'Совсем маленькие', description: '2016 год', path: '1.jpg'},
-    {title: 'Совсем маленькие', description: '2016 год', path: '2.jpg'},
-    {title: 'На прогулке', description: '2017 год', path: '3.jpg'},
+    {title: 'Совсем маленькие', description: '2016 год', avatarSize: 50, path: '1.jpg'},
+    {title: 'Совсем маленькие', description: '2016 год', avatarSize: 50, path: '2.jpg'},
+    {title: 'На прогулке', description: '2017 год', avatarSize: 50, path: '3.jpg'},
+    {title: 'На прогулке', description: '2017 год', avatarSize: 50, path: '3.jpg'},
   ];
+
+  onAvatarIncrease = () => this.photos.forEach(item => item.avatarSize = item.avatarSize + 1);
+
+  onAvatarDecrease() {
+    this.photos.forEach(item => item.avatarSize = item.avatarSize - 1);
+  }
 }
